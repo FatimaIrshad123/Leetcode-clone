1265592a759f8e4eda0c18255ae0c311eb2e1d56
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-11 18:34:58 +0500

    updated images, navbar and auth page

--- src/app/components/auth/page.tsx
+++ src/app/components/auth/page.tsx
@@ -1,8 +1,11 @@
+import Navbar from "../navbar/page";
 
 export default function AuthPage(){
     return (
-        <div>
-            AuthPage
+        <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+            <div className="max-w-7xl mx-auto">
+                <Navbar />
+            </div>
         </div>
     )
 }
--- src/app/components/navbar/page.tsx
+++ src/app/components/navbar/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
 
 export default function Navbar(){
     return (
-        <div>
-            Navbar
+        <div className="flex item-center justify-between sm:px-12 px-2 md:px-24">
+            <Link href={'/'} className="flex item-center justify-center h-20">
+                <img src="/logo.png" alt="leetclone" className="h-full"/>
+                </Link>
+                <div className="flex items-center">
+                    <button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent transition duration-300 ease-in-out">Sign In</button>
+                </div>
         </div>
     )
 }
