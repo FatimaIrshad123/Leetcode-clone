01a8df9f5ca60080e4361d518d0b8f77aa85919e
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-15 22:15:35 +0500

    updated loading

--- src/app/components/AuthPage.tsx
+++ src/app/components/AuthPage.tsx
@@ -2,9 +2,23 @@ import { useRecoilValue } from "recoil";
 import AuthModel from "./AuthModel";
 import Navbar from "./Navbar";
 import { authModelState } from "../atoms/authModelAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AuthPage(){
     const authmodel = useRecoilValue(authModelState)
+    const [pageLoading,setPageLoading] = useState(true);
+    const [user,loading,error] = useAuthState(auth)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (user) router.push('/')
+        if (!loading && !user) setPageLoading(false);
+    },[user,router,loading])
+
+    if (pageLoading) return null;
     return (
         <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
             <div className="max-w-7xl mx-auto">
