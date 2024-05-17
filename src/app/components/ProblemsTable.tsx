9e6481c3831f58fc76de82cdf279af9366e8459d
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-17 21:37:01 +0500

    Added youtube video

--- src/app/components/ProblemsTable.tsx
+++ src/app/components/ProblemsTable.tsx
@@ -2,10 +2,13 @@ import { BsCheckCircle } from "react-icons/bs";
 import { problems } from "../mockProblems/problems";
 import Link from "next/link";
 import { AiFillYoutube } from "react-icons/ai";
+import YouTube from "react-youtube";
+import { TiLockClosed } from "react-icons/ti";
+import { IoCloseSharp } from "react-icons/io5";
 
 export default function ProblemTable(){
     return (
-        
+        <>        
             <tbody className="text-white">
                 {problems.map((doc,idx) => {
                     const difficultyColor = doc.difficulty === "Easy" ? "text-dark-green-s": doc.difficulty === "Medium" ? "text-dark-yellow": "text-dark-pink";
@@ -40,5 +43,18 @@ export default function ProblemTable(){
                     )
                 })}
             </tbody>
+            <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center">
+                <div className="bg-black z-10 opacity-20 top-0 left-0 w-screen h-screen absolute"></div>
+                <div className="w-full z-50 h-full px-6 relative max-w-4xl">
+                    <div className="w-full h-full flex items-center justify-center relative">
+                        <div className="w-full relative">
+                            <IoCloseSharp fontSize={'35'} className='cursor-pointer absolute -top-16 right-0'/>
+                            <YouTube videoId={"4qYTqOiRMoM"} loading="lazy" iframeClassName="w-full min-h-[500px]"/>
+                        </div>
+                    </div>
+                </div>
+
+            </tfoot>
+            </>
     )
 }
