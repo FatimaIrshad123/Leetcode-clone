bd3735fd2a027c3069cac200cba721fd3d45971c
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-22 21:43:37 +0500

    Added workspace component

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -1,8 +1,11 @@
 import Topbar from "@/app/components/Topbar"
+import WorkSpace from "@/app/components/WorkSpace"
+
 export default function Problem(){
     return (
         <div>
             <Topbar problemPage/>
+            <WorkSpace />
         </div>
     )
 }
