212464a30a66ffc9c92f3d36ddf259cfda4de875
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-23 22:37:35 +0500

    Added vertical split

--- src/app/components/PlayGround.tsx
+++ src/app/components/PlayGround.tsx
@@ -1,9 +1,21 @@
 import PreferenceNavbar from "./PreferenceNavbar";
-
+import Split from "react-split";
+import CodeMirror from "@uiw/react-codemirror"
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
 export default function PlayGround(){
     return (
-        <>
+        <div className="flex flex-col bg-dark-layer-1 relative">
             <PreferenceNavbar />
-        </>
+            <Split className="h-[calc(100vh-34px)]" direction="vertical" sizes={[60,40]} minSize={60}>
+                <div className="w-full overflow-auto">
+                    <CodeMirror 
+                    value="const a-1"
+                    theme={vscodeDark}
+                    extensions={[javascript()]}/>
+                </div>
+
+            </Split>
+        </div>
     )
 }
