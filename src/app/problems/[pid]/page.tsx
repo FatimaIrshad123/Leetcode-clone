1eb256a8fd3a2f9a60befd5b15f36128558359b5
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-27 22:03:27 +0500

    updated problem page

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -2,6 +2,8 @@
 
 import Topbar from "@/app/components/Topbar"
 import WorkSpace from "@/app/components/WorkSpace"
+import { problems } from "@/app/utils/problems"
+import { notFound } from "next/navigation"
 
 export default function Problem(){
     return (
@@ -12,3 +14,28 @@ export default function Problem(){
     )
 };
 
+export async function getStaticPath() {
+    const paths = Object.keys(problems).map((key) => ({
+        params: {pid:key}
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}:any) {
+    const [pid] = params;
+    const problem = problems[pid];
+
+    if(!problem){
+        return {
+            notFound: true
+        }
+    }
+    return {
+        props: {
+            problem
+        }
+    }
+}
