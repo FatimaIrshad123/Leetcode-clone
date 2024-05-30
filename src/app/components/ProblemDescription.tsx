28ae96dd417429f8a31722395985e403843b80b0
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-30 23:46:36 +0500

    Updated problemDescription

--- src/app/components/ProblemDescription.tsx
+++ src/app/components/ProblemDescription.tsx
@@ -6,8 +6,8 @@ import { Problem } from "../mockProblems/problems";
 type problemPageProps = {
     problem : Problem
 }
-const ProblemDescription:React.FC<problemPageProps> = ({problem}) => {
-    
+const ProblemDescription:React.FC = ({problem}:any) => {
+    //console.log(problem)
     return (
         <div className='bg-dark-layer-1'>
         {/* TAB */}
@@ -48,55 +48,32 @@ const ProblemDescription:React.FC<problemPageProps> = ({problem}) => {
 
                     {/* Problem Statement(paragraphs) */}
                     <div className='text-white text-sm'>
-                        <p className='mt-3'>
-                            Given an array of integers <code>nums</code> and an integer <code>target</code>, return
-                            <em>indices of the two numbers such that they add up to</em> <code>target</code>.
-                        </p>
-                        <p className='mt-3'>
-                            You may assume that each input would have <strong>exactly one solution</strong>, and you
-                            may not use thesame element twice.
-                        </p>
-                        <p className='mt-3'>You can return the answer in any order.</p>
+                       <div dangerouslySetInnerHTML={{ __html : problem.problemStatement}}/>
                     </div>
 
                     {/* Examples */}
                     <div className='mt-4'>
-                        {/* Example 1 */}
-                        <div>
-                            <p className='font-medium text-white '>Example 1: </p>
+                        {problem?.examples.map((example:any,index:any) => {return (
+                        <div key={example.id}>
+                            <p className='font-medium text-white '>Example {index+1}: </p>
+                            {example.img && (
+                                <img src={example.img} alt="Image" className="mt-3"/>
+                            )}
                             <div className='example-card'>
                                 <pre>
-                                    <strong className='text-white'>Input: </strong> nums = [2,7,11,15], target = 9{" "}
+                                    <strong className='text-white'>Input: </strong> {example.inputText}
                                     <br />
-                                    <strong>Output:</strong> [0,1] <br />
-                                    <strong>Explanation:</strong>Because nums[0] + nums[1] == 9, we return [0, 1].
+                                    <strong>Output:</strong> {example.outputText} <br />
+                                    {example.explanation && (
+                                        <>
+                                        <strong>Explanation:</strong>{example.explanation}
+                                        </>    
+                                        )}
                                 </pre>
                             </div>
                         </div>
-
-                        {/* Example 2 */}
-                        <div>
-                            <p className='font-medium text-white '>Example 2: </p>
-                            <div className='example-card'>
-                                <pre>
-                                    <strong className='text-white'>Input: </strong> nums = [3,2,4], target = 6{" "}
-                                    <br />
-                                    <strong>Output:</strong> [1,2] <br />
-                                    <strong>Explanation:</strong>Because nums[1] + nums[2] == 6, we return [1, 2].
-                                </pre>
-                            </div>
-                        </div>
-                        {/* Example 3 */}
-                        <div>
-                            <p className='font-medium text-white '>Example 3: </p>
-                            <div className='example-card'>
-                                <pre>
-                                    <strong className='text-white'>Input: </strong> nums = [3,3], target = 6
-                                    <br />
-                                    <strong>Output:</strong> [0,1] <br />
-                                </pre>
-                            </div>
-                        </div>
+                        )
+                    })}
                     </div>
 
                     {/* Constraints */}
