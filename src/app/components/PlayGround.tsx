c0fbc9be4069b7b9101c897443f7a73be4ac3943
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-31 21:21:55 +0500

    updatded playground css

--- src/app/components/PlayGround.tsx
+++ src/app/components/PlayGround.tsx
@@ -36,9 +36,9 @@ export default function PlayGround({problem}:any){
                         <div className="flex">
                            {problem.examples.map((example:any,index:any)=> {
                             return (
-                            <div className="mr-2 items-center mt-2 text-white" key={example.id}onClick={() => setActiveTextCaseId(index)}>                           
+                            <div className="mr-2 items-center mt-2 " key={example.id}onClick={() => setActiveTextCaseId(index)}>                           
                                 <div className="flex flex-wrap items-center gap-y-4">
-                                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                                <div className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap ${activeTestCaseId === index ? "text-white": "text-gray-500"}`}>
                                 Case {index+1}
                                 </div>
                                  </div>
