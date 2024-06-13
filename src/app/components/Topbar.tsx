19386cdd691e50094aa039248dbb9b619cc83cd2
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-06-13 20:32:18 +0500

    Finished project

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -10,15 +10,15 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { BsList } from "react-icons/bs";
 import Timer from "./Timer";
 
+
 type TopbarProps = {
 	problemPage? : boolean;
 }
 
 export default function Topbar({ problemPage }: TopbarProps){
 	const [user] = useAuthState(auth);
-//	console.log(user)
 	const setAuthModalState = useSetRecoilState(authModelState)
-
+	
     return (
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
 			<div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto": ""}`}>
@@ -27,7 +27,7 @@ export default function Topbar({ problemPage }: TopbarProps){
 				</Link>		
 				{problemPage && (
 					<div className="flex items-center gap-4 flex-1 justify-center">
-						<div className="flex justify-center items-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+						<div className="flex justify-center items-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer" >
 							<FaChevronLeft />
 						</div>
 						<Link href="/" className="flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer">
@@ -36,7 +36,7 @@ export default function Topbar({ problemPage }: TopbarProps){
 							</div>
 							<p>Problem List</p>
 						</Link>
-						<div className="flex justify-center items-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+						<div className="flex justify-center items-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer" >
 							<FaChevronRight />
 						</div>
 
