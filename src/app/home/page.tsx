c14dad82de38dfd225c363579cd01973ae164b90
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-09-25 18:42:54 +0500

    Updated code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@
 import { RecoilRoot } from "recoil"
 import AuthPage from "../components/AuthPage"
 
-export default function(){
+export default function HomePage(){
     return (
         <RecoilRoot>
             <AuthPage />
